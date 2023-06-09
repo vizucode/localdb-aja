4e7d098ba49e9234025588fbe654f484a18a2afa
vizucode <havisikkubaru@gmail.com>
2023-06-09 20:12:29 +0700

    initializing

---
+++ README.md
@@ -0,0 +1,78 @@
+# Localdb-aja
+
+Localdb-aja is framework to make easier store, update, delete, find data to local file / localstorage.
+
+## Installation
+
+### using yarn package
+```bash
+yarn add localdb-aja
+```
+
+### using npm package
+```bash
+npm i localdb-aja
+```
+
+## Usage
+
+Please make sure you have to create the file .json first before starting!
+
+```javascript
+import localdb  from 'localdb-aja';
+
+/**
+ * Please add the constant path your database folder, ex: ./db/ 
+ * don't forget the backslash after the folder name
+ */
+var localDB = new localdb("./db")
+
+/**
+ * to store data in your local-database
+ * will return boolean
+ */
+var jsonData = `{"name":"yoromiya kasunagi"}`
+jsonData = JSON.parse(jsonData)
+
+localDB.store("product.json", jsonData)
+
+
+/**
+ * to update data in your local-database
+ * will return boolean
+ */
+var jsonData = `{"name":"kurisu amane"}`
+jsonData = JSON.parse(jsonData)
+
+localDB.update("product.json", jsonData, "name", "bang messi")
+
+/**
+ * to delete data in your local-database
+ * will return boolean
+ */
+localDB.destroy("product.json", "name", "yoromia kusanagi")
+
+/**
+ * to find all data in your local-database
+ * will return the array of data
+ */
+localDB.findAll("product.json")
+
+/**
+ * to find specific data in your local-database
+ * will return object of data
+ */
+localDB.find("product.json", "name", "yuta okkotsu")
+
+```
+
+## Contributing
+
+Pull requests are welcome. For major changes, please open an issue first
+to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ index.js
@@ -0,0 +1,113 @@
+import fs from 'fs';
+
+class LocalDB {
+    constructor(dbPath) {
+        this.dbPath = dbPath
+    }
+
+    checkFileExist(filePath){
+        if (fs.existsSync(filePath)) {
+            return true
+        }
+        return false
+    }
+
+    findAll(collection)  {
+        var path = this.dbPath+collection
+        if (!this.checkFileExist(path)) {
+            return false
+        }
+
+        var documents = JSON.parse(fs.readFileSync(path))
+        return Array.isArray(documents) ? documents : [documents];
+    }
+
+    // will query and return single data and there's a filter
+    find(collection, filter, value) {
+        var path = this.dbPath+collection
+        if (!this.checkFileExist(path)) {
+            return false
+        }
+
+        var documents = JSON.parse(fs.readFileSync(path))
+    
+        var singleDocument
+    
+        documents.forEach(document => {
+            if(filter == "" && value == "") {
+                return singleDocument = document
+            }
+    
+            if (document.hasOwnProperty(filter) && document[filter] == value) {
+                return singleDocument = document
+            }
+        })
+        
+        return singleDocument
+    }
+
+    // will return boolean
+    store(collection, documentRequest) {
+        var path = this.dbPath+collection
+        
+        if (typeof documentRequest != "object") {
+            return false
+        }
+
+        var data = this.findAll(collection)
+        if (typeof data == "boolean" && !data) {
+            return false
+        }
+
+        data.push(documentRequest)
+
+        var documentString = JSON.stringify(data)
+        fs.writeFileSync(path, documentString)
+
+        return true
+    }
+
+    // will return boolean
+    update(collection, documentRequest, filter, value) {
+        if ((filter == "" || filter == undefined) && (value == "" || value == undefined)) {
+            return false
+        }
+
+        var result = this.destroy(collection, filter, value)
+        if (!result){
+            return false
+        }
+        var result = this.store(collection, documentRequest)
+        if (!result){
+            return false
+        }
+
+        return true
+    }
+
+    // will return boolean
+    destroy(collection, filter, value) {
+        var path = this.dbPath+collection
+        if (!this.checkFileExist(path)) {
+            return false
+        }
+
+        var documents = this.findAll(collection)
+        documents.forEach((val, idx) => {
+            if (val[filter] == value) {
+                documents.splice(idx, 1)
+            }
+        })
+
+        var documentString = JSON.stringify(documents)
+        fs.writeFileSync(path, documentString)
+
+        return true
+    }
+
+    checkHealth(collection) {
+        return collection
+    }
+}
+
+export default LocalDB
